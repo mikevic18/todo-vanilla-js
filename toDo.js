66bd0e44428e4d317b976a2448ac692caeb0278d
Michael T <56955591+mikevic18@users.noreply.github.com>
2023-04-03 09:59:17 +0100

    Updated checkbox method, started updateTodo

--- toDo.js
+++ toDo.js
@@ -1,27 +1,42 @@
-
-async function render(data){
-    const list = document.getElementById('list');
-    list.innerHTML = '';
-    data.forEach(todo => {
-        const li = document.createElement('li');
-        li.innerHTML = todo;
-        const checkbox = document.createElement('input');
-        checkbox.type = 'checkbox';
-        checkbox.name = 'name';
-        checkbox.value = 'value';
-        checkbox.id = `todo-${todo}`;
+async function render(data) {
+    const list = document.getElementById("list");
+    list.innerHTML = "";
+    data.forEach((todo) => {;
+        const li = document.createElement("li");
+        li.innerHTML = todo.text;
+        const checkbox = document.createElement("input");
+        checkbox.type = "checkbox";
+        if (todo.done) checkbox.checked = true;
+        checkbox.name = "name";
+        checkbox.id = `todo-${todo.id}`;
+        checkbox.addEventListener("change", saveData);
         list.appendChild(checkbox);
         list.appendChild(li);
     });
 }
+async function saveData() {
+    updateToDo();
+    const data = JSON.stringify(data);
+    localStorage.setItem("data", data);
+}
+function updateToDo() {
+    const checkboxes = document.querySelectorAll("input[type=checkbox]");
+    checkboxes.forEach((checkbox) => {
+        const id = checkbox.id.split("-")[1];
+        const todo = data.find((todo) => todo.id == id);
+        todo.done = checkbox.checked;
+    });
+}
 
-
-function addTodo(){
-    const todo = document.getElementById('todo').value;
+function addTodo() {
+    const todo = document.getElementById("todo").value;
     const data = [todo];
     render(data);
 }
 
-
-data = ['todo1', 'todo2', 'todo3'];
+let data = [
+    { id: 1, text: "todo1", done: true },
+    { id: 2, text: "todo2", done: false },
+    { id: 1, text: "todo3", done: false },
+];
 render(data);
